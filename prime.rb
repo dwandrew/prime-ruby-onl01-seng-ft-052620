8b84e6aa97bc0b7f50c418c403e23242146b4d72
Daniel Andrew <danwandrew@gmail.com>
2020-05-27 12:09:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 # Add  code here!
 def prime?(num)
+  if num <0;
+      false
+  end
   i=1
   while i<num
-    if num <0
-      false
-    elsif i !=1 && i!=num && num%1 ==0
+    if i !=1 && i!=num && num%1 ==0
       false;
-    
     end
     i+=1;
   end
